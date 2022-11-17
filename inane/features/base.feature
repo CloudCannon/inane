da8761ac32a66503660a39fa02574f56abae49c8
Liam Bigelow <40188355+bglw@users.noreply.github.com>
2022-11-17 13:42:00 +1300

    Cull everything

--- inane/features/base.feature
+++ inane/features/base.feature
@@ -2,10 +2,4 @@ Feature: Base Tests
 
     Scenario: Default message
         When I run my program
-        Then I should see "Hello World" in stdout
-
-    Scenario: Custom message
-        Given I have the environment variables:
-            | INANE_MESSAGE | Hullo |
-        When I run my program
-        Then I should see "Hullo" in stdout
+        Then I should see "There is nothing to do" in stdout
--- inane/src/lib.rs
+++ inane/src/lib.rs
@@ -1,21 +1,11 @@
-pub use options::{InaneInboundConfig, InaneOptions};
-mod logging;
-mod options;
-
-pub struct InaneState {
-    pub options: InaneOptions,
-}
+pub struct InaneState {}
 
 impl InaneState {
-    pub fn new(options: InaneOptions) -> Self {
-        Self { options }
+    pub fn new() -> Self {
+        Self {}
     }
 
     pub fn run(&mut self) {
-        let log = &self.options.logger;
-        log.status(&format!("Running Inane v{}", self.options.version));
-        log.v_info("Running in verbose mode");
-
-        log.info(format!("Message: {}", self.options.message));
+        println!("There is nothing to do.")
     }
 }
